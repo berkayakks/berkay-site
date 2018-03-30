ca58264810165d70c3371b5bfd3e32e41df932d8
Berkay Akkuş <37518427+berkayakks@users.noreply.github.com>
2018-03-30 20:32:52 +0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace berkay_akkuş.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
